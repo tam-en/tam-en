97116522d21ca17c18de67ee64a36289d036bb9e
tam-en <tamis@leftelbow.com>
2019-03-07 15:06:58 -0800

    secondary pages up yadda yadda

--- js/script.js
+++ js/script.js
@@ -1,75 +1,40 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 	console.log("DOM fully loaded and parsed");
+
+
+	// GLOBAL VARIABLES
+	const sectionsForListening = [
+		["about-contact", "about-bubble", "about.html"],
+		["writing-editing", "writing-bubble", "editorial.html"],
+		["web-dev", "web-dev-bubble", "web_dev.html"],
+		["print-design", "print-bubble", "print_design.html"]
+	];
+
+	function listenAndDo(section) {
+		document.getElementById(section[0]).addEventListener('mouseover', function() {
+			document.getElementById(section[0]).style.color = "#5C3877";
+			document.getElementById(section[1]).style.opacity = "0.5";
+		});
+		document.getElementById(section[0]).addEventListener('mouseout', function() {
+			document.getElementById(section[0]).style.color = "black";
+			document.getElementById(section[1]).style.opacity = "1";
+		});
+		document.getElementById(section[0]).addEventListener('click', function() {
+			window.location.href = section[2];
+		});
+		document.getElementById(section[1]).addEventListener('click', function() {
+			window.location.href = section[2];
+		});		
+		document.getElementById(section[1]).addEventListener('mouseover', function() {
+			document.getElementById(section[0]).style.color = "#5C3877";
+			document.getElementById(section[1]).style.opacity = "0.5";
+		});
+		document.getElementById(section[1]).addEventListener('mouseout', function() {
+			document.getElementById(section[0]).style.color = "black";
+			document.getElementById(section[1]).style.opacity = "1";
+		});
+	};
+
+	sectionsForListening.forEach(listenAndDo);
+
 });
-
-// functions for dimming and brightening elements with links
-
-function dimAbout() {
-	document.getElementById("about-bubble").style.opacity = "0.5";
-	document.getElementById("about-contact").style.opacity = "0.5";
-};
-
-function brightenAbout() {
-	document.getElementById("about-bubble").style.opacity = "1";
-	document.getElementById("about-contact").style.opacity = "1";	
-};
-
-function dimWriting() {
-	document.getElementById("writing-editing").style.opacity = "0.5";
-	document.getElementById("writing-bubble").style.opacity = "0.5";
-};
-
-function brightenWriting() {
-	document.getElementById("writing-editing").style.opacity = "1";
-	document.getElementById("writing-bubble").style.opacity = "1";
-};
-
-function dimWebDev() {
-
-};
-
-function dimDesign() {
-
-};
-
-
-// turn on listeners for elements
-
-function listenersOn() {
-	document.getElementById("about-contact").addEventListener('mouseover', function() {
-		dimAbout();
-	});
-	document.getElementById("about-contact").addEventListener('mouseout', function() {
-		brightenAbout();
-	});	
-	document.getElementById("about-bubble").addEventListener('mouseover', function() {
-		dimAbout();
-	});
-	document.getElementById("about-bubble").addEventListener('mouseout', function() {
-		brightenAbout();
-	});
-
-	document.getElementsByClassName("writing-editing").addEventListener('mouseover', function(){
-		dimWriting();
-	});
-	document.getElementsByClassName("writing-bubble").addEventListener('mouseout', function(){
-		brightenWriting();
-	});	
-
-	document.getElementById("web-dev-bubble").addEventListener('click', function(){
-		console.log("web dev tickles")
-	});
-	document.getElementById("web-dev").addEventListener('click', function(){
-		console.log("web-dev tickles")
-	});	
-	document.getElementById("print-design").addEventListener('click', function(){
-		console.log("print design tickles")
-	});
-	document.getElementById("print-bubble").addEventListener('click', function(){
-		console.log("print design tickles")
-	});						
-};
-
-listenersOn();
-
-
