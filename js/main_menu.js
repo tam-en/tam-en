5b6a32a52dc39a3b9e8cd61dd13f232095356dce
tam-en <tamis@leftelbow.com>
2019-04-26 14:47:38 -0700

    another attempt at home entry bug fix

--- js/main_menu.js
+++ js/main_menu.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	let menuText = [];
 
 	let makeMenuItems = function(item) {
-		if(fileName != item[1]) {
+		if((fileName != item[1]) && (fileName != "")) {
 			menuText.push(`<a href="${item[1]}.html">${item[0]}</a>`);
 		} else {
 			menuText.push(`<a href="${item[1]}.html" class="activeLink">${item[0]}</a>`);
